45822ed89432991a1a992a5aeafb8d811449a4d6
GabrielOved <gabriel.ovedenie1999@gmail.com>
2026-03-08 22:04:26 +0200

    Minor improvement to the Email Generation for the Web Scraping project and getting comments more closely aligned with PEP 8 standards for all 3 projects

--- CsvCompare/FileComparison.py
+++ CsvCompare/FileComparison.py
@@ -11,56 +11,57 @@ date = str(now.date())
 def csv_comparison(source_file_left=f'source_{date}.csv', target_file_right=f'target_{date}_csv',
                    result_file=f'output{date}.csv'):
     try:
-        source = pd.read_csv(source_file_left) # Convert data from the Source CSV to a DataFrame
-        target = pd.read_csv(target_file_right) # Convert data from the Target CSV to a DataFrame
-        prompt = "Press 'y' or 'n' if you wish to proceed or stop.\n" # Prompt for column check
-        left_difference_con_num = len(source.columns)-len(target.columns) # The column number difference between the Source and Target Dataframe
-        right_difference_con_num = len(target.columns) - len(source.columns) # The column number difference between the Target and Source Dataframe
-        source_difference = set(source.columns.difference(target.columns)) # The column difference between the Source and Target Dataframe
-        target_difference = set(target.columns.difference(source.columns)) # The column difference between the Target and Source Dataframe
-        if set(source.columns) != set(target.columns) and 0 in {left_difference_con_num, right_difference_con_num}: # Check for a difference in columns when the number is the same for both Source and Target
+        source = pd.read_csv(source_file_left)  # Convert data from the Source CSV to a DataFrame
+        target = pd.read_csv(target_file_right)  # Convert data from the Target CSV to a DataFrame
+        prompt = "Press 'y' or 'n' if you wish to proceed or stop.\n"  # Prompt for column check
+        left_difference_con_num = len(source.columns)-len(target.columns)  # The column number difference between the Source and Target Dataframe
+        right_difference_con_num = len(target.columns) - len(source.columns)  # The column number difference between the Target and Source Dataframe
+        source_difference = set(source.columns.difference(target.columns))  # The column difference between the Source and Target Dataframe
+        target_difference = set(target.columns.difference(source.columns))  # The column difference between the Target and Source Dataframe
+        if set(source.columns) != set(target.columns) and 0 in {left_difference_con_num, right_difference_con_num}:  # Check for a difference in columns when the number is the same for both Source and Target
             print(f"Columns are different between {source_file_left} and {target_file_right}\n"
-                f"{source_file_left}: \033[1m{tuple(source.columns)}\033[0m\n{target_file_right}: \033[1m{tuple(target.columns)}\033[0m") # Output explaining the column difference
-            function = input(prompt).lower().strip() # input for the prompt to continue or stop that allows spaces or uppercase
-            if function == 'n': # check to stop if the user input n or N
+                  f"{source_file_left}: \033[1m{tuple(source.columns)}\033[0m\n{target_file_right}: \033[1m{tuple(target.columns)}\033[0m")  # Output explaining the column difference
+            function = input(prompt).lower().strip()  # input for the prompt to continue or stop that allows spaces or uppercase
+            if function == 'n':  # check to stop if the user input n or N
                 exit()
-        elif left_difference_con_num > 0: # Check for the number of columns difference when there is already a column difference between Source and Target
-            print(f"{source_file_left} file has these {left_difference_con_num} more column/s {source_difference} than {target_file_right}") # Output explaining the column number difference
-            function = input(prompt).lower().strip() # input for the prompt to continue or stop that allows spaces or uppercase
-            if function == 'n': # check to stop if the user input n or N
+        elif left_difference_con_num > 0:  # Check for the number of columns difference when there is already a column difference between Source and Target
+            print(f"{source_file_left} file has these {left_difference_con_num} more column/s {source_difference} than {target_file_right}")  # Output explaining the column number difference
+            function = input(prompt).lower().strip()  # input for the prompt to continue or stop that allows spaces or uppercase
+            if function == 'n':  # check to stop if the user input n or N
                 exit()
-        elif right_difference_con_num > 0: # Check for the number of columns difference when there is already a column difference between Target and Source
-            print(f"{target_file_right} file has these {right_difference_con_num} more column/s {target_difference} than {source_file_left}") # Output explaining the column number difference
-            function = input(prompt).lower().strip() # input for the prompt to continue or stop that allows spaces or uppercase
-            if function == 'n': # check to stop if the user input n or N
+        elif right_difference_con_num > 0:  # Check for the number of columns difference when there is already a column difference between Target and Source
+            print(f"{target_file_right} file has these {right_difference_con_num} more column/s {target_difference} than {source_file_left}")  # Output explaining the column number difference
+            function = input(prompt).lower().strip()  # input for the prompt to continue or stop that allows spaces or uppercase
+            if function == 'n':  # check to stop if the user input n or N
                 exit()
-        number = '1' # initialization of a variable used to check for the existence of the output file
-        print(f"🕑 Comparing the {source_file_left} and {target_file_right} files") # print to keep the user informed throughout the process
-        diff = pd.merge(source, target, how="outer", indicator="Exist") # merge between all the values of the two Dataframes
+        number = '1'  # initialization of a variable used to check for the existence of the output file
+        print(f"🕑 Comparing the {source_file_left} and {target_file_right} files")  # print to keep the user informed throughout the process
+        diff = pd.merge(source, target, how="outer", indicator="Exist")  # merge between all the values of the two Dataframes
         """using the Exist indicator to filer only the difference:
         left_only -> the difference between Source and Target with the output being the Source difference
         right_only -> the difference between Target and Source with the output being the Target difference
         !=both -> the differences that are not located in both Dataframes with the output being a mix from Source and Target"""
         diff = diff.loc[diff["Exist"] == "left_only"]
-        diff.drop(labels="Exist", axis="columns", inplace=True) # drop of the Exist column after being used to compare
-        for i in range(10): # for loop name check for CSV output file
-            while exists(result_file): # Check for the existence of the CSV output file
-                result_file = 'output' + '_' + date + '_' + number + '.csv' # name of the CSV output file if the while statement is still True
-                number = chr(ord(number) + 1) # incrementation for the name check
+        diff.drop(labels="Exist", axis="columns", inplace=True)  # drop of the Exist column after being used to compare
+        for i in range(10):  # for loop name check for CSV output file
+            while exists(result_file):  # Check for the existence of the CSV output file
+                result_file = 'output' + '_' + date + '_' + number + '.csv'  # name of the CSV output file if the while statement is still True
+                number = chr(ord(number) + 1)  # incrementation for the name check
             else:
-                diff.to_csv(path_or_buf=f"{result_file}", encoding='utf-8', index=False, header=True) # result dataframe is made into a CSV file
-                break # break out of the for loop
-        if diff.empty: # Check if there were any differences
-            print("✅ Comparison file generated successfully with NO mismatches") # print if no differences were found between the two files
-        elif len(diff.index) == 1: # Check if there is only 1 difference
-            print(f"❌ Comparison file generated successfully with {len(diff.index)} mismatch") # print if there is only 1 difference between the two files
+                diff.to_csv(path_or_buf=f"{result_file}", encoding='utf-8', index=False, header=True)  # result dataframe is made into a CSV file
+                break  # break out of the for loop
+        if diff.empty:  # Check if there were any differences
+            print("✅ Comparison file generated successfully with NO mismatches")  # print if no differences were found between the two files
+        elif len(diff.index) == 1:  # Check if there is only 1 difference
+            print(f"❌ Comparison file generated successfully with {len(diff.index)} mismatch")  # print if there is only 1 difference between the two files
         else:
-            print(f"❌ Comparison file generated successfully with {len(diff.index)} mismatches") # print if there are many differences between the two files
+            print(f"❌ Comparison file generated successfully with {len(diff.index)} mismatches")  # print if there are many differences between the two files
 
-    except Exception as e: # Exception handling
-        tb = traceback.extract_tb(e.__traceback__) # traceback object to retrieve the line triggering the exception
+    except Exception as e:  # Exception handling
+        tb = traceback.extract_tb(e.__traceback__)  # traceback object to retrieve the line triggering the exception
         first_frame = tb[-1]
-        print(f"❌ The following Exception occurred, line {first_frame.lineno} in {first_frame.name}\n{e}") # print of the exception and the line that triggered it
+        print(f"❌ The following Exception occurred, line {first_frame.lineno} in {first_frame.name}\n{e}")  # print of the exception and the line that triggered it
+
 
 # final call to generate the comparison file
 csv_comparison(source_file_left=f"2026-02-18_top_250_imdb_movies.csv", target_file_right=f"2026-02-03_top_250_imdb_movies.csv",
--- WebScarpingThermoenergeticDefect/ThermoenergeticIssueScraping.py
+++ WebScarpingThermoenergeticDefect/ThermoenergeticIssueScraping.py
@@ -1,19 +1,19 @@
-import requests # http requests
-from bs4 import BeautifulSoup # web scraping
-import smtplib # Send mail
-import os # get from .env file
-from dotenv import load_dotenv # load .env file
-import sys # used only for the Schedule option, otherwise can be commented out
+import requests  # http requests
+from bs4 import BeautifulSoup  # web scraping
+import smtplib  # Send mail
+import os  # get from .env file
+from dotenv import load_dotenv  # load .env file
+import sys  # used only for the Schedule option, otherwise can be commented out
 # email body
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import datetime # system date and time
-import re # regex used to find the correct substring
+import datetime  # system date and time
+import re  # regex used to find the correct substring
 
 # Current date used for the email summary
 now = datetime.datetime.now().replace(microsecond=0)
 
-# user agent required to establish the connection to the API
+# user agent required to establish the connection to the webpage API
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -22,76 +22,78 @@ headers = {
 
 
 class ThermoenergeticIssue:
-    def __init__(self, url, street_name): # initialization of the url and street name parameters
+    def __init__(self, url, street_name):  # initialization of the url and street name parameters
         self.url = url
         self.street_name = street_name
 
     # function that handles the address extraction
     def extract_issue(self):
-        print(f"✅ Extracting the Thermoenergetic issue for the {self.street_name} street") # print to keep the user informed throughout the process
-        cont = '' # placeholder for the mail contents inside this function
-        cont += ('<b>Affected Street:</b>\n'+'<br>'+'-'*80+'<br>') # first line in the email if the street is present on the webpage
-        response = requests.get(self.url, headers=headers) # call to the site API to approve the connection
-        content = response.content # content of the webpage
-        filters = BeautifulSoup(content, "html.parser") # function that reads html content of the webpage
-        address = re.compile(fr"({self.street_name} - \s*.+?)(?:\s*•|\s*Deficienta)") # search for the specified street based on the html body
-        for i, tag in enumerate(filters.find("div", id="ST")): # for loop to narrow down the search in the html body
-            for f, adr in enumerate(address.findall(tag.text)): # for loop to check each line for the street name
-                cont += ((adr + '\n' + '<br>') if self.street_name in tag.text else '') # the address/addresses is added to the contents of the email
-        return cont # returning the contents for the email function
+        print(f"✅ Extracting the Thermoenergetic issue for the {self.street_name} street")  # print to keep the user informed throughout the process
+        cont = ''  # placeholder for the mail contents inside this function
+        cont += ('<b>Affected Street:</b>\n'+'<br>'+'-'*80+'<br>')  # first line in the email if the street is present on the webpage
+        response = requests.get(self.url, headers=headers)  # call to the site API to approve the connection
+        content = response.content  # content of the webpage
+        filters = BeautifulSoup(content, "html.parser")  # function that reads html content of the webpage
+        address = re.compile(fr"({self.street_name} - \s*.+?)(?:\s*•|\s*Deficienta)")  # search for the specified street using a regex that takes into account addresses at the start/middle/end of the cell
+        for i, tag in enumerate(filters.find("div", id="ST")):  # for loop to narrow down the search in the html body
+            for f, adr in enumerate(address.findall(tag.text)):  # for loop to check each line for the street name
+                cont += ((adr + '\n' + '<br>') if self.street_name in tag.text else '')  # the address/addresses is added to the contents of the email
+        return cont  # returning the contents for the email function
 
     # function that generates and sends the email
     def email_generation(self):
         try:
-            cont = self.extract_issue() # call to the function that handles the address extraction
-            content = '' # email placeholder inside this function
-            content += cont # the contents from the address extraction are added to the value inside this function
-            content += ('-'*80 + '<br>') # line to make the email look more professional
+            cont = self.extract_issue()  # call to the function that handles the address extraction
+            content = ''  # email placeholder inside this function
 
-            msg = MIMEMultipart() # Call to function needed for the email Subject, From, To
+            msg = MIMEMultipart()  # Call to function needed for the email Subject, From, To
 
-            if self.street_name not in cont: # Check to see if the street is present in the webpage
-                msg['Subject'] = f'[Automated Email] No Thermoenergetic issue for the {self.street_name} street' # Replacement of the email Subject if the street is not present on the webpage
-                content = (('<br>' + '-'*80 + '<br>') +
-                           f'No Thermoenergetic issue have been reported for the {self.street_name} street at {now}'
-                           + ('<br>' + '-'*80 + '<br>')) # Replacement of the Mail body if the street is not present on the webpage
+            if self.street_name in cont:  # Check to see if the street is present in the webpage
+                msg['Subject'] = f'[Automated Email] Addresses with a Thermoenergetic defect for {self.street_name}'   # email Subject if the street is present in the webpage
+                content += cont  # the contents from the address extraction are added to the value inside this function
+                content += ('-' * 80 + '<br>')  # line to make the email look more professional
             else:
-                msg['Subject'] = f'[Automated Email] Addresses with a Thermoenergetic defect for {self.street_name}' # email Subject if the street is present in the webpage
-            # For the connection to gmail using a third party app, the 2 Factor Authentication needs to be set up for that gmail account
+                msg['Subject'] = f'[Automated Email] No Thermoenergetic defect for the {self.street_name} street'  # Replacement of the email Subject if the street is not present on the webpage
+                content = (('<br>' + '-'*80 + '<br>') +
+                           f'No Thermoenergetic defect has been reported for the {self.street_name} street at {now}'
+                           + ('<br>' + '-'*80 + '<br>'))  # Replacement of the Mail body if the street is not present on the webpage
+
+            # For the connection to gmail using a third party app, the 2-Factor Authentication needs to be set up for that gmail account
             # After that search App Password in the Search Google Account section and once selected type the App name (ex.Python) then press Create
-            load_dotenv() # call to load the .env file
-            SERVER = os.getenv("SERVER") # the smtp server from the .env file set to gmail as the default value
-            PORT = os.getenv("PORT") # port number from the .env file set to a default value
-            FROM = os.getenv("FROM_EMAIL") # From email from the .env file that needs to be set not having a default value
-            TO = os.getenv("TO_EMAIL") # To email from the .env file that needs to be set not having a default value
-            PASSWORD = os.getenv("EMAIL_PASSWORD") # Password for the From email located in the .env file that needs to be set not having a default value
+            load_dotenv()  # call to load the .env file
+            SERVER = os.getenv("SERVER")  # the smtp server from the .env file set to gmail as the default value
+            PORT = os.getenv("PORT")  # port number from the .env file set to a default value
+            FROM = os.getenv("FROM_EMAIL")  # From email from the .env file that needs to be set not having a default value
+            TO = os.getenv("TO_EMAIL")  # To email from the .env file that needs to be set not having a default value
+            PASSWORD = os.getenv("EMAIL_PASSWORD")  # Password for the From email located in the .env file that needs to be set not having a default value
 
-            msg['From'] = FROM # assigning the FROM mail in .env to the MIME variable
-            msg['To'] = TO # assigning the TO mail in .env to the MIME variable
-            print("✅ Composing Email") # print to keep the user informed throughout the process
+            msg['From'] = FROM  # assigning the FROM mail in .env to the MIME variable
+            msg['To'] = TO  # assigning the TO mail in .env to the MIME variable
+            print("✅ Composing Email")  # print to keep the user informed throughout the process
 
-            msg.attach(MIMEText(content, 'html')) # attaching the body of the Email to the accompanying Subject, From, To
+            msg.attach(MIMEText(content, 'html'))  # attaching the body of the Email to the accompanying Subject, From, To
 
-            print('✅ Initiating Connection') # print to keep the user informed throughout the process
+            print('✅ Initiating Connection')  # print to keep the user informed throughout the process
 
-            server = smtplib.SMTP(SERVER, PORT) # function to connect to the email server using the port
-            server.set_debuglevel(0) # setting the debug level to 0 to not show any traceback messages
-            server.ehlo() # initiating command for the connection
-            server.starttls() # starting the secured connection
-            server.login(FROM, PASSWORD) # login using the FROM email and PASSWORD from the .env file
-            server.sendmail(FROM, TO, msg.as_string()) # sending one of the two composed emails based on the presence of the street in the webpage
+            server = smtplib.SMTP(SERVER, PORT)  # function to connect to the email server using the port
+            server.set_debuglevel(0)  # setting the debug level to 0 to not show any traceback messages
+            server.ehlo()  # initiating command for the connection
+            server.starttls()  # starting the secured connection
+            server.login(FROM, PASSWORD)  # login using the FROM email and PASSWORD from the .env file
+            server.sendmail(FROM, TO, msg.as_string())  # sending one of the two composed emails based on the presence of the street in the webpage
 
-            print("✅ Email Sent") # print to keep the user informed throughout the process
+            print("✅ Email Sent")  # print to keep the user informed throughout the process
 
-            server.quit() # closing the connection to the server
+            server.quit()  # closing the connection to the server
 
         except Exception as e:  # Exception handling
             print(f"❌ The following Exception occurred:\n{e}")  # print of the exception and the line that triggered it
 
             return self
 
-#ONLY NEEDED IF THE SCHEDULE OPTION IS IMPLEMENTED OTHERWISE COMMENT ALONG WITH THE IMPORT
-street_name = sys.argv[2] # Argument when calling the function from a related  app (ex.cmd)
 
-# Call to the class to generate the email, the street_name=street_name can be replaced if the Schedule option is not selected with ex.street_name=Moșilor
+# ONLY NEEDED IF THE SCHEDULE OPTION IS IMPLEMENTED OTHERWISE COMMENT ALONG WITH THE IMPORT
+street_name = sys.argv[2]  # Argument when calling the function from a related  app (ex.cmd)
+
+# Call to the class to generate the email, the street_name=street_name can be replaced if the Schedule option is not selected with ex.street_name="Moșilor"
 ThermoenergeticIssue(url="https://cmteb.ro/functionare_sistem_termoficare.php", street_name=street_name).email_generation()
